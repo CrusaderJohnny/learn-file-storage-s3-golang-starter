5086a6c92fff5bba007a7de63c748d750f61af38
CrusaderJohnny <17ark.leonardis@gmail.com>
2026-02-18 11:37:58 -0700

    WIP videouploader

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,7 +1,105 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	r.Body = http.MaxBytesReader(w, r.Body, 1<<30)
+	videoIdString := r.PathValue("videoID")
+	videoId, err := uuid.Parse(videoIdString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "invalid ID", err)
+		return
+	}
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "no valid token found", err)
+		return
+	}
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "could not validate token", err)
+		return
+	}
+	video, err := cfg.db.GetVideo(videoId)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "video not found", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "unauthorized access", errors.New("unauthorized access"))
+		return
+	}
+	file, header, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "issue forming file from video: ", err)
+		return
+	}
+	defer file.Close()
+	videoType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "issue parsing media type", err)
+		return
+	}
+	if videoType != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "media type must be video/mp4", nil)
+		return
+	}
+	tempFile, err := os.CreateTemp("", "tubely-upload.mp4")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "issue creating temp file", err)
+		return
+	}
+	defer tempFile.Close()
+	defer os.Remove(tempFile.Name())
+	_, err = io.Copy(tempFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "issue copying video to temporary upload location", err)
+		return
+	}
+	_, err = tempFile.Seek(0, io.SeekStart)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "issue resetting pointer to beginning", err)
+		return
+	}
+	saveName := make([]byte, 32)
+	_, err = rand.Read(saveName)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "issue generating random filename", err)
+		return
+	}
+	key := hex.EncodeToString(saveName) + ".mp4"
+
+	_, err = cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{
+		Bucket:      aws.String(cfg.s3Bucket),
+		Key:         aws.String(key),
+		Body:        tempFile,
+		ContentType: aws.String(videoType),
+	})
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "issue uploading video to s3", err)
+		return
+	}
+
+	dataURL := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, key)
+	video.VideoURL = &dataURL
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "issue updating video", err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, video)
+}
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
@@ -20,6 +23,7 @@ type apiConfig struct {
 	s3Region         string
 	s3CfDistribution string
 	port             string
+	s3Client         *s3.Client
 }
 
 func main() {
@@ -75,6 +79,12 @@ func main() {
 		log.Fatal("PORT environment variable is not set")
 	}
 
+	bucketClient, err := config.LoadDefaultConfig(context.Background(), config.WithRegion(s3Region))
+	if err != nil {
+		log.Fatalf("Couldn't load S3 client: %v", err)
+	}
+	clientConfig := s3.NewFromConfig(bucketClient)
+
 	cfg := apiConfig{
 		db:               db,
 		jwtSecret:        jwtSecret,
@@ -85,6 +95,7 @@ func main() {
 		s3Region:         s3Region,
 		s3CfDistribution: s3CfDistribution,
 		port:             port,
+		s3Client:         clientConfig,
 	}
 
 	err = cfg.ensureAssetsDir()
